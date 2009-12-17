b1aaa154f40e027771471fd2a42998063d031cce
Sunteya <sunteya@Sunteyas-MacBook-Air.local>
2009-12-17 09:40:21 +0800

    extract generator commons methods

--- generators/fume/fume_generator.rb
+++ generators/fume/fume_generator.rb
@@ -1,6 +1,8 @@
 class FumeGenerator < Rails::Generator::Base
 
-
+  attr_accessor :gen
+  delegate :source_path, :destination_path, :manifest, :after_generate, :replay, :to => :gen
+  
   def initialize(runtime_args, runtime_options = {})
     super
     
@@ -11,10 +13,6 @@ class FumeGenerator < Rails::Generator::Base
     gen_spec = Fume::Gen::Spec.new(gen_name, gen_path, gen_path)
     
     @gen = gen_spec.klass.new(runtime_args, runtime_options)
-  end
-
-  def manifest
-    @gen.manifest
   end
 end
 
@@ -28,6 +26,20 @@ module Fume
 
       def source_root_pathname
         Pathname.new(self.source_root)
+      end
+      
+      def cp_files(pattern)
+        Pathname.glob(source_root_pathname.join(pattern)) do |source_path|
+          relative_path = source_path.relative_path_from(source_root_pathname)
+          destination_path = destination_root_pathname.join(relative_path)
+
+          if source_path.directory?
+            @manifest_util.directory relative_path unless destination_path.exist?
+          else
+            @manifest_util.directory relative_path.dirname unless destination_path.dirname.exist?
+            @manifest_util.file relative_path.to_s, relative_path.to_s
+          end
+        end
       end
     end
     
--- generators/fume/simple_theme/simple_theme_gen.rb
+++ generators/fume/simple_theme/simple_theme_gen.rb
@@ -1,33 +1,17 @@
-class FumeSimpleThemeGenerator < Fume::Gen::Base
+class SimpleThemeGen < Fume::Gen::Base
 
   def manifest
     @manifest_util = Rails::Generator::Manifest.new(self)
-    
+
     # generate_images
-    cp_files source_root_pathname.join("images"), destination_root_pathname.join("public", "images")
+    cp_files File.join("public", "images", "**", "*")
     
     # generate_stylesheets
-    cp_files source_root_pathname.join("stylesheets"), destination_root_pathname.join("public", "stylesheets"), [ "*.css" ]
-
+    cp_files File.join("public", "stylesheets", "*.css")
+    
     # generate_javascripts
-    cp_files source_root_pathname.join("javascripts"), destination_root_pathname.join("public", "javascripts")
+    cp_files File.join("public", "javascripts", "**", "*")
 
     @manifest_util
-  end
-
-  def cp_files(source_pathname, target_pathname, patterns = ["**/*"])
-    patterns.each do |pattern|
-      Pathname.glob(source_pathname.join(pattern)) do |source_path|
-        relative_path = source_path.relative_path_from(source_pathname)
-        target_path = target_pathname.join(relative_path)
-
-        if source_path.directory?
-          @manifest_util.directory target_path.relative_path_from(orig_target_pathname) unless target_path.exist?
-        else
-          @manifest_util.directory target_path.dirname.relative_path_from(orig_target_pathname) unless target_path.dirname.exist?
-          @manifest_util.file source_path.relative_path_from(orig_source_pathname).to_s, target_path.relative_path_from(orig_target_pathname).to_s
-        end
-      end
-    end
   end
 end
