04a083c2fef5411e4e8df39c7e386de45faf443c
Sunteya <Sunteya@gmail.com>
2010-04-15 11:04:54 +0800

    reorganize simple theme template files.

--- generators/fume/simple_theme/simple_theme_gen.rb
+++ generators/fume/simple_theme/simple_theme_gen.rb
@@ -2,14 +2,8 @@ class SimpleThemeGen < Fume::Gen::Base
 
   def manifest
     record do |m|
-      # generate_images
-      cp_files m, File.join("public", "images", "**", "*")
-
-      # generate_stylesheets
+      cp_files m, File.join("public", "themes", "**", "*")
       cp_files m, File.join("public", "stylesheets", "*.css")
-
-      # generate_javascripts
-      cp_files m, File.join("public", "javascripts", "**", "*")
     end
   end
 end
