22468d45312b77ed327f08a2ba67660b82d06b34
Sunteya <Sunteya@gmail.com>
2011-08-30 11:23:19 +0800

    remove simple_theme generator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
 ## 0.7.0:
   - add action_attr_accessor method
   - depend rails_i18n
+  - rename simple\_theme to simple_nav and rewirte
--- rails_generators/fume/simple_theme/simple_theme_gen.rb
+++
@@ -1,9 +0,0 @@
-class SimpleThemeGen < Fume::Gen::Base
-
-  def manifest
-    record do |m|
-      cp_files m, File.join("public", "themes", "**", "*")
-      cp_files m, File.join("public", "stylesheets", "*.css")
-    end
-  end
-end
